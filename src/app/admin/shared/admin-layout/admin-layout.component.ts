ad7a3e7dd808dd4391417f9706060d201a31e606
Gregtstu <boltnev.grisha@yandex.ru>
2024-01-16 21:29:27 +0300

    add dist

--- src/app/admin/shared/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/admin-layout/admin-layout.component.ts
@@ -1,8 +1,6 @@
 import {Component, EventEmitter, Output, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
-import {MatSidenav, MatSidenavContainer, MatSidenavModule} from "@angular/material/sidenav";
-import {childRoutes} from "../child-routes";
-import {MatButtonModule} from "@angular/material/button";
+import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
 import { AuthService } from 'src/app/services/auth.service';
 
 
@@ -10,42 +8,24 @@ import { AuthService } from 'src/app/services/auth.service';
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss'],
-  // standalone: true,
-  // imports: [MatSidenavModule, MatButtonModule],
 })
 export class AdminLayoutComponent {
   @Output() sideNavToggled = new EventEmitter<void>();
   @ViewChild(MatSidenavContainer) sidenavContainer!: MatSidenavContainer;
   @ViewChild('snav') sideNav!: MatSidenav;
-  showFiller = false;
-  sideNavDefaultOpened = true;
-  showFullMenu = true;
-  isExpanded = true;
-  closedWidth = 90;
-  openedWidth = 250;
-  isMobile!: boolean;
-  sideNavMode: 'side' | 'over' = 'side';
-  hasBackdrop: boolean = false;
-  toolBarHeight = 64;
-  routes = childRoutes;
+
+  public   localesList = [
+    { code: 'en-US', label: 'English' },
+    { code: 'ru', label: 'Русский' }
+  ];
   constructor(
-    public auth: AuthService, 
+    public auth: AuthService,
     private rout: Router
     ) {}
 
   ngOnInit() {
-    console.log(this.auth.isAuthenticated());
-    
-  }
 
-  toggleSidebar(): void {
-    this.sideNavToggled.emit();
   }
-
-  onLoggedout() {
-    this.rout.navigate(['/']);
-  }
-
   logout(){
     this.auth.logout();
     this.rout.navigate(['/admin', 'login'])
